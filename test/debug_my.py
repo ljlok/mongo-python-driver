9d0356aa68304d64620cd7f00689c170121987b4
ljlok <ljlok2008@gmail.com>
2016-06-23 17:15:17 +0800

    add my debug file for debuging

---
+++ test/debug_my.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# encoding: utf-8
+_author_ = 'jialiang'
+_email_ = 'jialiang.liu@nexusguard.com'
+
+
+'''
+Debug function file
+'''
+import os, sys
+lib_path = os.path.abspath(os.path.join("."))
+sys.path.append(lib_path)
+print lib_path
+from pymongo.mongo_client import MongoClient
+
+def debug(mongo_addr):
+    connection = MongoClient(mongo_addr)
+    print connection
+
+if __name__ == '__main__':
+    mongo_addr = "mongodb://127.0.0.1:27017"
+    print debug(mongo_addr)
